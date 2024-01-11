34cd5979db3573fadb9b4fa289f66a3e20bb69a2
alan.shishido <alan.shishido@fpf.br>
2024-01-11 18:57:52 -0400

    lesson 2

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
+    const {method, url} = req
+    console.log(method, url)
+
     return res.end('Agora sim')
 })
 
